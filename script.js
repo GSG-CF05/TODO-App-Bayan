a19da796df5dad11ca9d3a3bce510ab3daf4756d
bayan2002 <bayanabdelbaryq@gmail.com>
2022-03-05 13:42:47 +0200

    the ability to add a new todo to the list and local storage relates #14

---
+++ script.js
@@ -0,0 +1,115 @@
+let input = document.querySelector(".input");
+let submit = document.querySelector(".add");
+let tasksDiv = document.querySelector(".tasks");
+
+// Empty Array To Store The Tasks
+let arrayOfTasks = [];
+
+// Check if Theres Tasks In Local Storage
+if (localStorage.getItem("tasks")) {
+  arrayOfTasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+getDataFromLocalStorage();
+
+// Add Task
+submit.onclick = function () {
+  if (input.value !== "") {
+    addTaskToArray(input.value); // Add Task To Array Of Tasks
+    input.value = ""; // Empty Input Field
+  }
+};
+
+
+
+
+
+
+
+
+
+
+function addTaskToArray(taskText) {
+  // Task Data
+  const task = {
+    id: Date.now(),
+    title: taskText,
+    completed: false,
+  };
+  // Push Task To Array Of Tasks
+  arrayOfTasks.push(task);
+  // Add Tasks To Page
+  addElementsToPageFrom(arrayOfTasks);
+  // Add Tasks To Local Storage
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function addElementsToPageFrom(arrayOfTasks) {
+  // Empty Tasks Div
+  tasksDiv.innerHTML = "";
+  // Looping On Array Of Tasks
+  arrayOfTasks.forEach((task) => {
+    // Create Main Div
+    let div = document.createElement("div");
+    div.className = "task";
+    // Check If Task is Done
+    if (task.completed) {
+      div.className = "task done";
+    }
+    div.setAttribute("data-id", task.id);
+    let span0 =document.createElement("span")
+    span0.textContent = `${task.title}`
+    span0.style.width ="500px"
+    div.appendChild(span0);
+    // Create Delete Button
+    let span = document.createElement("span");
+    span.className = "del";
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.setAttribute("class", "fa-solid fa-trash-can");
+
+    span.appendChild(deleteIcon);
+    ///create check button
+    let span2 = document.createElement("span");
+    span2.className = "check";
+
+    let checkIcon = document.createElement("i");
+    checkIcon.setAttribute("class", "fa-solid fa-check");
+
+    span2.appendChild(checkIcon);
+    //////create edit button
+    let span3 = document.createElement("span");
+    span3.className = "edit";
+
+    let editIcon = document.createElement("i");
+    editIcon.setAttribute("class", "fa-solid fa-pen");
+
+    span3.appendChild(editIcon);
+    // Append Button To  Div
+    div.appendChild(span);
+    div.appendChild(span2);
+    div.appendChild(span3);
+    
+    // Add Task Div To Tasks Container
+    tasksDiv.appendChild(div);
+
+      function newFunction() {
+          console.log(document.createTextNode(task.title));
+      }
+  });
+}
+function addDataToLocalStorageFrom(arrayOfTasks) {
+  window.localStorage.setItem("tasks", JSON.stringify(arrayOfTasks));
+}
+
+function getDataFromLocalStorage() {
+  let data = window.localStorage.getItem("tasks");
+  if (data) {
+    let tasks = JSON.parse(data);
+    addElementsToPageFrom(tasks);
+  }
+}
+function deleteTaskWith(taskId) {
+  arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
